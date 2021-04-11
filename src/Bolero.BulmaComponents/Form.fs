7c1476f97d6aa14d570477fa2e77ed664b840b49
Kojo Amoasi <kojo12331@hotmail.co.uk>
2021-04-11 20:22:40 +0100

    add Checkbox, Textarea and Radio form elements - amend Input to have bind event

--- src/Bolero.BulmaComponents/Form.fs
+++ src/Bolero.BulmaComponents/Form.fs
@@ -30,6 +30,7 @@ module Input =
         {
             Label: string option
             Placeholder: string option
+            Text: string
             Type: AlphaNumericInputType option
             Color: ComponentColor option
             Size: Size option
@@ -41,6 +42,7 @@ module Input =
             IsStatic: bool
             LeftIcon: Node option
             RightIcon: Node option
+            BindEvent: string -> unit
         }
         interface INodeable with
             member this.ToNode() =
@@ -77,6 +79,8 @@ module Input =
                             attr.disabled this.IsDisabled
                             attr.readonly this.IsReadonly
                             attr.placeholder (Option.toObj this.Placeholder)
+
+                            bind.input.string this.Text this.BindEvent
                         ]
                         
                         cond this.LeftIcon <| function
@@ -93,6 +97,7 @@ module Input =
         {
             Label = None
             Placeholder = None
+            Text = ""
             Type = None
             Color = None
             Size = None
@@ -104,7 +109,17 @@ module Input =
             IsStatic = false
             LeftIcon = None
             RightIcon = None
+            BindEvent = ignore
         }
+
+    let withText t model =
+        { model with Text = t }
+
+    let withLabel label model =
+        { model with Label = Some label }
+
+    let withPlaceholder placeholder model =
+        { model with Placeholder = Some placeholder }
 
     let withInputType inputType model =
         { model with Type = Some inputType }
@@ -139,17 +154,204 @@ module Input =
     let withRightIcon n model =
         { model with RightIcon = Some n }
 
+    let bindEvent func model =
+        { model with BindEvent = func }
+
+    let createInputWithEvent func =
+        createInput() |> bindEvent func
+
+    let createInputWithPlaceholder placeholder =
+        createInput() |> withPlaceholder placeholder
+
 module Radio =
-    begin end
+    type RadioModel<'T when 'T : comparison> =
+        {
+            RadioButtonMap: Map<'T, string>
+            RadioButtonContent: Map<'T, Node>
+            GroupName: string option
+            BindEvent: 'T -> unit
+        }
+        interface INodeable with
+            member this.ToNode() =
+                let buttons = Map.toList this.RadioButtonMap
+                div [ attr.``class`` "field" ] [
+                    div [ attr.``class`` "control" ] [
+                        forEach buttons <| fun (key, value) ->
+                            label [ attr.``class`` "radio" ] [
+                                input [
+                                    attr.``type`` "radio"
+                                    attr.name (Option.toObj this.GroupName)
+                                    bind.change.string value (fun _ -> this.BindEvent key)
+                                ]
+
+                                cond (Map.tryFind key this.RadioButtonContent) <| function
+                                | Some n -> n
+                                | None -> empty
+                            ]
+                    ]
+                ]
+
+    let createRadioGroup() =
+        {
+            RadioButtonMap = Map.empty
+            RadioButtonContent = Map.empty
+            GroupName = None
+            BindEvent = ignore
+        }
+
+    let withMap map model =
+        { model with RadioButtonMap = map }
+
+    let withContent map model =
+        { model with RadioButtonContent = map }
+
+    let withName name model =
+        { model with GroupName = Some name }
+
+    let setBindEvent func model =
+        { model with BindEvent = func }
 
 module Checkbox =
-    begin end
+    let createCheckbox content =
+        div [ attr.``class`` "field" ] [
+            div [ attr.``class`` "control" ] [
+                label [ attr.``class`` "checkbox" ] [
+                    input [ attr.``type`` "checkbox" ]
 
-module File =
-    begin end
+                    content
+                ]
+            ]
+        ]
+
+    let createCheckboxBind content callback isChecked =
+        div [ attr.``class`` "field" ] [
+            div [ attr.``class`` "control" ] [
+                label [ attr.``class`` "checkbox" ] [
+                    input [
+                        attr.``type`` "checkbox"
+                        bind.checked isChecked callback
+                    ]
+
+                    content
+                ]
+            ]
+        ]
 
 module TextArea =
-    begin end
+    type State =
+        | Hover
+        | Focus
+        | Active
+        override this.ToString() =
+            match this with
+            | Hover -> "is-hovered"
+            | Focus -> "is-focused"
+            | Active -> "is-active"
+
+    type TextAreaModel =
+        {
+            Label: string option
+            Placeholder: string option
+            Text: string
+            Rows: int option
+            Color: ComponentColor option
+            Size: Size option
+            State: State option
+            IsLoading: bool
+            IsDisabled: bool
+            IsReadonly: bool
+            IsFixedSize: bool
+            BindEvent: string -> unit
+            
+        }
+        interface INodeable with
+            member this.ToNode() =
+                let classes =
+                    [
+                        Some "textarea"
+                        Option.map colorToString this.Color
+                        this.Size |> Option.map (fun x -> x.ToStringWithoutNormal())
+                        Option.map string this.State
+                        Option.boolMap "has-fixed-size" this.IsFixedSize
+                    ]
+                    |> List.choose id
+
+                div [ attr.``class`` "field" ] [
+                    div [
+                        [
+                            Some "control"
+                            Option.boolMap "is-loading" this.IsLoading
+                        ]
+                        |> List.choose id
+                        |> attr.classes
+                    ] [
+                        cond this.Label <| function
+                        | Some str -> label [ attr.``class``  "label"] [ text str ]
+                        | None -> empty
+
+                        textarea [
+                            attr.classes classes
+                            attr.disabled this.IsDisabled
+                            attr.readonly this.IsReadonly
+                            attr.rows (this.Rows |> Option.map string |> Option.toObj)
+                            attr.placeholder (Option.toObj this.Placeholder)
+
+                            bind.input.string this.Text this.BindEvent
+                        ] []
+                    ]
+                ]
+
+    let createTextArea() =
+        {
+            Label = None
+            Placeholder = None
+            Text = ""
+            Rows = None
+            Color = None
+            Size = None
+            State = None
+            IsLoading = false
+            IsDisabled = false
+            IsReadonly = false
+            IsFixedSize = false
+            BindEvent = ignore
+        }
+
+    let withText t model =
+        { model with Text = t }
+
+    let withLabel label model =
+        { model with Label = Some label }
+
+    let withPlaceholder placeholder model =
+        { model with Placeholder = Some placeholder }
+
+    let withRows noRows model =
+        { model with Rows = Some noRows }
+
+    let withColor c model =
+        { model with Color = Some c }
+
+    let withSize s model =
+        { model with Size = Some s }
+
+    let withState s model =
+        { model with State = Some s }
+
+    let setLoading model =
+        { model with IsLoading = true }
+
+    let setDisabled model =
+        { model with IsDisabled = true }
+
+    let setReadonly model =
+        { model with IsReadonly = true }
+
+    let setFixedSize model =
+        { model with IsFixedSize = true }
+
+    let bindEvent func model =
+        { model with BindEvent = func }
 
 module Select =
     begin end
